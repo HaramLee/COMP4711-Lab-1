860e397a40611df5812c5ee0741388fa7b693245
Haram Lee <haram.lee@hotmail.ca>
2016-09-09 22:47:22 -0700

    added method to calculate grades and method for printing output

--- Student.php
+++ Student.php
@@ -13,6 +13,7 @@
  */
 class Student {
     
+    //The constructor of the class
     function __construct() {
         $this->surname = '';
         $this->first_name = '';
@@ -20,11 +21,31 @@ class Student {
         $this->grades = array();
     }
     
+    //Mutator methods to add emails
     function add_email($which, $address){
-        $this->email[$which] = $address;
+        $this->emails[$which] = $address;
     }
     
+    //Mutator methods to add grades
     function add_grade($grade){
-        $this->grade[] = $grade;
+        $this->grades[] = $grade;
+    }
+    
+    //Calculating the average grade 
+    function average(){
+        $this = 0;
+        foreach ($this->grades as $value){
+            $total += $value;
+        }
+        return $total / count($this->grades);
+    }
+    
+    function toString(){
+        $result = $this->first_name . ' ' . $this->surname;
+        $result .= ' (' . $this->average() . ")\n";
+        foreach ($this->emails as $which => $what )
+            $result .= $which . ': ' . $what . "\n";
+        $result .= "\n";
+        return '<pre>' . $result . '</pre>';
     }
 }
